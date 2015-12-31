f7c2d29756b009cfb4c8b2bbcbb3a1284f04515b
schas002 <schas002@users.noreply.github.com>
2015-12-31 18:42:11 +0300

    Add print, input, yes/no, many related functions
    Full list of affected functions: p p2b yn yn2i i iai s2i i2s

--- js-function-golf.js
+++ js-function-golf.js
@@ -14,3 +14,74 @@ function $(id) {
 function _(address) {
   document.body.appendChild(document.createElement('script')).src = address;
 }
+
+/**
+ * p - the print to console function
+ * @param data to print
+ */
+function p(data) {
+  console.log(data);
+}
+
+/**
+ * p2b - the print to browser function
+ * @param data to print
+ */
+function p2b(data) {
+  alert(data);
+}
+
+/**
+ * yn - the yes/no function
+ * @param question to ask
+ * @return the answer to the question
+ */
+function yn(question) {
+  return confirm(question);
+}
+
+/**
+ * yn2i - the yes/no to integer function
+ * @param question to ask
+ * @return the answer to the question converted to integer
+ */
+function yn2i(question) {
+  return yn(question)? 1 : 0;
+}
+
+/**
+ * i - the input function
+ * @param question to ask
+ * @return the answer to the question
+ */
+function i(question) {
+  return prompt(question);
+}
+
+/**
+ * iai - the input an integer function
+ * @param question to ask
+ * @return the answer to the question converted to integer
+ */
+
+function iai(question) {
+  return i(question).parseInt();
+}
+
+/**
+ * s2i - the string to integer function
+ * @param data to convert
+ * @return the data converted to integer
+ */
+function s2i(data) {
+  return data.parseInt();
+}
+
+/**
+ * i2s - the item to string function
+ * @param data to convert
+ * @return the data converted to string
+ */
+function i2s(data) {
+  return data.toString();
+}
