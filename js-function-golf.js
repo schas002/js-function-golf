4f05e9a2a4f55b931916d195b155b9d871b0106b
schas002 <schas002@users.noreply.github.com>
2015-12-31 17:20:55 +0300

    Add $() and _()

--- js-function-golf.js
+++ js-function-golf.js
@@ -1,3 +1,16 @@
+/**
+ * $ - the HTML element lookup function.
+ * @param id of the element
+ * @return the corresponding element
+ */
 function $(id) {
-return document.getElementById(id);
+  return document.getElementById(id);
+}
+
+/**
+ * _ - the include function.
+ * @param address of the JavaScript file to be included
+ */
+function _(address) {
+  document.body.appendChild(document.createElement('script')).src = address;
 }
